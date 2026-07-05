091db32feb8439b5bf651abb954d842f4de39387
Alma Dafa <almadafa@Almas-MacBook-Air.local>
2026-07-05 13:01:27 +0200

    Remove duplicate background image from App.jsx

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -6,27 +6,12 @@ import LoadingPage from './Components/LoadingPage'
 import ErrorPage from './Components/ErrorPage'
 import ResultsPage from './Components/ResultsPage'
 import HowItWorksPage from './Components/HowItWorksPage'
-import background from './assets/background.png'
 import CitationGraph from './Components/CitationGraph'
 
 function App() {
   return (
     <BrowserRouter>
       <div style={{ position: "relative", minHeight: "100vh", width: "100%", display: "flex", flexDirection: "column" }}>
-
-        <div style={{
-          position: "absolute", top: 0, left: 0, right: 0, height: "700px",
-          backgroundImage: `url(${background})`,
-          backgroundSize: "1200px auto",
-          backgroundPosition: "center 80px",
-          backgroundRepeat: "no-repeat",
-          zIndex: 0
-        }} />
-        <div style={{
-          position: "absolute", top: 0, left: 0, right: 0, height: "700px",
-          background: "rgba(245,245,245,0.92)",
-          zIndex: 0
-        }} />
 
         <div style={{ position: "relative", zIndex: 1, flex: 1, display: "flex", flexDirection: "column" }}>
           <Header />
